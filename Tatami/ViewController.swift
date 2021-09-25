dd7930c04a46cee21f9442d8b5fbd36b59dfb1b0
Layne Johnson <layne@findum.com>
2021-09-25 15:19:07 -0400

    Enable plane detection, configure plane anchor

--- Tatami/ViewController.swift
+++ Tatami/ViewController.swift
@@ -16,24 +16,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Show plane detection feature points
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        
-        // Create node
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            
-            // Set node position
-            diceNode.position = SCNVector3(0, 0, -0.1)
-            
-            //
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
+//        // Create a new scene
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//
+//        // Create node
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//
+//            // Set node position
+//            diceNode.position = SCNVector3(0, 0, -0.1)
+//
+//            // Add node to scene
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,6 +44,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // Enable horizontal plane detection
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -77,5 +83,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // This code is triggered when a horizontal plane is detected
+        
+        if anchor is ARPlaneAnchor {
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            // Convert the dimensions of anchor into scene plane
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            // Create plane node
+            let planeNode = SCNNode()
+            
+            // Position node
+            planeNode.position = SCNVector3(planeAnchor.center.y, 0, planeAnchor.center.z)
+            
+        } else {
+            return
+        }
     }
 }
